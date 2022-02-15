d439778e39585f271a57499d446beafb05744e98
Jayden Pak <jppak86@gmail.com>
2022-02-15 13:17:56 -0500

    deployment

--- snake.js
+++ snake.js
@@ -1,8 +1,4 @@
 
-// Initialize with init function
-	// add restart function
-  // Define variables
-
 let score = 0;
 let scoreSelect = document.querySelector(".score");
 let snakeLength = 1;
@@ -13,18 +9,12 @@ let intervalTime = 0;
 let interval = 0;
 let cells = document.querySelectorAll(".cell-type")
 
-  // let snakeStartLo;
-  // let applelLo;
-
-// Draw a game board 
-	// Define a small cell using CSS  grid
-  // make a function name "buildBoard"
-  // assign a cell as a div by using for loop
 buildBoard()
 init()
 
 function buildBoard() {
   const gridContainerEl = document.getElementById("grid-container");
+  gridContainerEl.innerHTML = ""
   for (let i = 0; i < 900; i++){
     const cellEl = document.createElement("div");
     cellEl.setAttribute("class", "cell");
@@ -65,9 +55,17 @@ function gameOver() {
 }
 
 function keepMoving(){
+  console.log(startPosition)
   startPosition.pop();
   startPosition.unshift(startPosition[0] + direction);
   startPosition.forEach(i => document.getElementById(`${i}`).style.backgroundColor = "black");
+  const cells = document.querySelectorAll(".cell-type")
+  cells.forEach(cell => {
+  if(!startPosition.includes(parseInt(cell.id))){
+    cell.style.backgroundColor = "#0c81b8";
+  }
+  })
+    
   eatApple();
   
 }
@@ -115,44 +113,3 @@ function restartGame() {
 buildBoard();
 
 }
-
-
-// function moveByArrow() {
-//   document.addEventListener('keydown', (e) => {
-//     e = e || window.event;
-//     if (e.key === 'ArrowUp') {
-//       postion1 = startPosition[0] +  30
-      
-//     } else if (e.key === 'ArrowDown') {
-//       startPosition[0](el -= 30)
-//     } else if (e.key === 'ArrowLeft') {
-//       startPosition.forEach(el -= 1)
-//     } else if (e.key === 'ArrowRight') {
-//       startPosition.forEach(el += 1)
-//     }
-//   });
-
-// }
-// moveByArrow()
-// function nextpostion () {
-//   let position = [];
-//   if(randomNum === position[3]){
-//     document.getElementById(randomNum).style.backgroundColor = "black"
-//   } 
-
-// }
-
-
-      // define "render" function which makes snake keeps moving to forward direction if there is no arrow key input
-    // Stop moving when hit one of four walls
-    // Change direction with arrow keys
-      //add event listener "keydown" for each arrow key buttons
-      //make a "changeDirection" function
-
-// Scores increases by 1 every time snake eats a fruit
-  // add a function name "newAppleLo" to locate a new apple
-
-// Game is over when snake hit the wall or hit itself
-  // make a "GameOver" function which stop the game with alert message
-//  Restart the game with restart button
-  // make a "restart" function
